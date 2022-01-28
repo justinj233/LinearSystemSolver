6d7b87f530794f943fc5c7b5012acc399b78451c
justinj233 <84936869+justinj233@users.noreply.github.com>
2022-01-27 20:48:16 -0800

    Add files via upload

---
+++ src/linearSystemTesting.cpp
@@ -0,0 +1,57 @@
+#include "linearSystemClass.h"
+#include "matrixClass.h"
+#include <iostream>
+#include <conio.h>
+
+int main()
+{
+	std::cout << "Linear system solver: This program solves equations of the form Ax=b" << std::endl << std::endl;
+	matrix A;
+	matrix x;
+	matrix b;
+	matrix residual;
+
+	std::cout << "Enter details for the matrix A:" << std::endl;
+	A.getMatrix();
+
+	std::cout << "Enter details for the matrix b:" << std::endl;
+	b.getMatrix(A.returnrows(), 1);
+
+	matrix ACopy;
+	matrix bCopy;
+
+	ACopy.copyMatrix(A);
+	bCopy.copyMatrix(b);
+
+	std::cout << std::endl << "----------------------------------------------------------------------------------------------------------" << std::endl;
+	std::cout << "Equation to be solved:" << std::endl << std::endl;
+
+	ACopy.displayMatrix();
+	std::cout << "********************************************" << std::endl << std::endl << "x" << std::endl << std::endl << "============================================" << std::endl << std::endl;
+	bCopy.displayMatrix();
+
+	std::cout << std::endl << "----------------------------------------------------------------------------------------------------------" << std::endl;
+
+
+	linearSystem mySystem(A, b);
+	mySystem.GESolver();
+	x = mySystem.returnSolution();
+
+
+	std::cout << std::endl << "Solution:" << std::endl << std::endl;
+	x.displayMatrix();
+
+	std::cout << std::endl << std::endl << "----------------------------------------------------------------------------------------------------------" << std::endl;
+	
+	std::cout << std::endl << "Residual vector (b - A*solution): " << std::endl << std::endl;
+	matrix Ax = ACopy * x;
+	residual = bCopy - Ax;
+	residual.displayMatrix();
+	std::cout << std::endl;
+
+
+
+	std::cout << "press anything to exit" << std::endl;
+	_getch();
+	return 0;
+}
